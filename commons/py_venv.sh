0eb1e4b1ae923af677f079e85cc8fd9aeed8c1fb
Gumshoe Media Inc <65322760+gumshoe00@users.noreply.github.com>
2022-03-07 17:48:29 -0700

    added poetry init

--- commons/py_venv.sh
+++ commons/py_venv.sh
@@ -4,4 +4,9 @@ pyenv local 3.9.1
 python -m venv ./.venv
 . .venv/bin/activate
 python -m pip install --upgrade pip
+pip install poetry
+poetry init
+deactivate
+poetry shell
+
 echo '.venv' >> .gitignore
